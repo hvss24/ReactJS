c1a45df9ab674ce87d33cd104b134c91e5a1db79
hvss24 <sisodiyaharshvardhan999@gmail.com>
2026-01-14 11:12:07 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+console.log(name);//undefined
+
+var name = "Harsh";
+console.log(name);//Harsh
+
+// console.log(name);//gices error that cannot access name before initialization
+// let name = "Harsh";
+// console.log(name);
